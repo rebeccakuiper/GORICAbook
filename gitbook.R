2d7c9083666169376921a75128d06307c82467ea
Rebecca Kuiper <r.m.kuiper@uu.nl>
2026-02-19 13:30:25 +0100

    Test making gitbook

---
+++ gitbook.R
@@ -0,0 +1,11 @@
+#https://cjvanlissa.github.io/gitbook-demo/getgitbook.html
+
+# install.packages("bookdown")
+# install.packages("tinytex")
+# tinytex::install_tinytex()
+# git2r::config(global = TRUE, 
+#               user.name = "Rebecca Kuiper", user.email = "r.m.kuiper@uu.nl")
+
+#To create a new chapter, you must follow two steps: 
+# 1) Create the file, and 
+# 2) Include it in the list of chapters. -- _bookdown.yml
